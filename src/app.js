ad7c876475df59629b2f91d54e825241e0b52247
hamster mainer <porhek555@gmail.com>
2025-12-12 03:07:07 +0300

    Initial backend commit for Render deploy

--- src/app.js
+++ src/app.js
@@ -54,7 +54,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: 'https://fucknJava.github.io/oil-market', // Конкретный origin
+  origin: '*', 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
